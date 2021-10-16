e1ef2e2936fd6a44e8c0dd600ae504261a6fc4d5
Asma <asmaa.h.askar@gmail.com>
2021-10-16 02:38:52 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.typing', {
+    // Waits 1000ms after typing "First"
+    strings: ['Developer...', 'Freelancer...' , 'Photographer...'],
+    typeSpeed: 150,
+    backSpeed: 60,
+    showCursor: false,
+    loop: true,
+    loopCount: Infinity,
+
+});
